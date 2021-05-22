f1abcb2d7b6ddefa0ccc62cd899d265f15d18d15
shaquille david <shaquilledavid@hotmail.com>
2021-05-22 17:23:34 -0400

    merge strings

---
+++ Easy/1768. Merge Strings Alternately.py
@@ -0,0 +1,75 @@
+"""
+https://leetcode.com/problems/merge-strings-alternately/
+
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+ 
+
+Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+Example 2:
+
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+Example 3:
+
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q 
+merged: a p b q c   d
+"""
+
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        lst1 = [i for i in word1]
+        lst2 = [x for x in word2]
+        
+        if len(lst1) == len(lst2):
+            biglst = []
+            i = 0
+            while i < len(lst1):
+                biglst.append(lst1[i])
+                biglst.append(lst2[i])
+                i = i + 1
+            return ''.join(biglst)
+        
+        else:
+            biglst1 = []
+            
+            # x tells us what our cutoff index is
+            if len(lst1) < len(lst2):
+                x = len(lst1)
+            if len(lst2) < len(lst1):
+                x = len(lst2)
+            i = 0
+            while i < x:
+                biglst1.append(lst1[i])
+                biglst1.append(lst2[i])
+                i = i + 1
+            
+            if x == len(lst1):
+                biglst1.append(word2[i:])
+            if x == len(lst2):
+                biglst1.append(word1[i:])
+            
+            return ''.join(str(e) for e in biglst1)
+        
