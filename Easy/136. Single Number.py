42204d5b15b30c4267af8a31a58324bdedb33476
Shaquille David <shaquilledavid@hotmail.com>
2022-02-15 23:39:25 -0500

    completed 136 daily challenege

---
+++ Easy/136. Single Number.py
@@ -0,0 +1,28 @@
+"""
+https://leetcode.com/problems/single-number/
+
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+
+Example 1:
+
+Input: nums = [2,2,1]
+Output: 1
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+Output: 4
+Example 3:
+
+Input: nums = [1]
+Output: 1
+"""
+def singleNumber(self, nums: List[int]) -> int:
+    i = 0
+    while i < len(nums):
+        if nums[i] in nums[i+1:] or nums[i] in nums[:i]:
+            i += 1
+        else:
+            return nums[i]
