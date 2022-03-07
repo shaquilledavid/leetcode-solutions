098335595d137f96cd385934d84dea8bd065b087
Shaquille David <shaquilledavid@hotmail.com>
2022-03-07 12:29:35 -0500

    completed 468

--- Medium/468. Validate IP Address.py
+++ Medium/468. Validate IP Address.py
@@ -1,14 +1,53 @@
+"""
+https://leetcode.com/problems/validate-ip-address/
+
+Given a string queryIP, return "IPv4" if IP is a valid IPv4 address, "IPv6" if
+IP is a valid IPv6 address or "Neither" if IP is not a correct IP of any type.
+
+A valid IPv4 address is an IP in the form "x1.x2.x3.x4" where 0 <= xi <= 255
+and xi cannot contain leading zeros. For example, "192.168.1.1" and "192.168.1.0"
+are valid IPv4 addresses but "192.168.01.1", while "192.168.1.00" and "192.168@1.1"
+are invalid IPv4 addresses.
+
+A valid IPv6 address is an IP in the form "x1:x2:x3:x4:x5:x6:x7:x8" where:
+
+1 <= xi.length <= 4
+xi is a hexadecimal string which may contain digits, lower-case English letter
+('a' to 'f') and upper-case English letters ('A' to 'F').
+Leading zeros are allowed in xi.
+For example, "2001:0db8:85a3:0000:0000:8a2e:0370:7334" and "2001:db8:85a3:0:0:8A2E:0370:7334"
+are valid IPv6 addresses, while "2001:0db8:85a3::8A2E:037j:7334" and "02001:0db8:85a3:0000:0000:8a2e:0370:7334"
+are invalid IPv6 addresses.
+ 
+Example 1:
+
+Input: queryIP = "172.16.254.1"
+Output: "IPv4"
+Explanation: This is a valid IPv4 address, return "IPv4".
+Example 2:
+
+Input: queryIP = "2001:0db8:85a3:0:0:8A2E:0370:7334"
+Output: "IPv6"
+Explanation: This is a valid IPv6 address, return "IPv6".
+Example 3:
+
+Input: queryIP = "256.256.256.256"
+Output: "Neither"
+Explanation: This is neither a IPv4 address nor a IPv6 address.
+"""
+
 def validIPAddress(queryIP):
-        """
-        :type queryIP: str
-        :rtype: str
-        """
-        split = queryIP.split('.')
-        if len(split) == 1:
-            split = queryIP.split(':')
-            return validateIPv6(split)
+    """
+    :type queryIP: str
+    :rtype: str
+    """
+    split = queryIP.split('.')
+    if len(split) == 1:
+        split = queryIP.split(':')
+        return validateIPv6(split)
         
-        return validateIPv4(split)
+    return validateIPv4(split)
+
 
 def validateIPv4(nums):
     if len(nums) != 4:
@@ -25,11 +64,14 @@ def validateIPv4(nums):
 
     return "IPv4"
 
+
 def validateIPv6(nums):
     if len(nums) != 8:
         return "Neither"
     
     for num in nums:
+        if len(num) > 4:
+            return "Neither"
         try:
             hex(int(num, 16))
         except:
