c87dfbe3775eb7a027eb952f2aa8248a39c77761
Shaquille David <shaquilledavid@hotmail.com>
2024-04-13 13:28:25 -0400

    1512 done with mathematical solution

--- Easy/1512. Number of Good Pairs.py
+++ Easy/1512. Number of Good Pairs.py
@@ -15,3 +15,17 @@ def numIdenticalPairs(self, nums):
         
 
     return sum(len(value) for value in d.values())
+
+
+def numIdenticalPairs2(self, nums):
+    numseen = []
+    count = 0
+    for num in nums:
+        if num not in numseen:
+            count_of_number = nums.count(num)
+            count += count_of_number * (count_of_number - 1) // 2
+            numseen.append(num)    
+        else:
+            pass
+        
+    return count
