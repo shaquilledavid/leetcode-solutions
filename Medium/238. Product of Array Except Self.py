33fa2f489c99d3592b886d51a3ba4d8bee38b8c0
Shaquille David <shaquilledavid@hotmail.com>
2024-05-07 19:41:33 -0400

    optimized 238 to have O(n) runtime

--- Medium/238. Product of Array Except Self.py
+++ Medium/238. Product of Array Except Self.py
@@ -1,23 +1,36 @@
-def productExceptSelf(nums):
+def productExceptSelf(self, nums):
     """
     :type nums: List[int]
     :rtype: List[int]
     """
-    res = []
     
-    i = 0 
+    res = []
+    index = 0 
 
-    while i < len(nums):
-        product = 1
-        j = 0
-        while j < len(nums):
-            if j == i:
-                pass
-            else:
-                product *= nums[j]
-            j += 1
-            
-        res.append(product)
-        i += 1
+    
+    #compute the prefix and postfix products at each
+    prefix = [1]
+    postfix = [1]
+    product = 1
+
+    #prefix work
+    while index < len(nums):
+         product *= nums[index]
+         prefix.append(product)
+         index += 1
+        
+    #postfix work
+    prod = 1
+    ind = len(nums) - 1
+    while ind >= 0:
+        prod *= nums[ind]
+        postfix.insert(0, prod)
+        ind -= 1
+        #value going into the result array at each index with be the product of the prefix at index*postfix at index+1
+
+    n = 0
+    while n < len(nums):
+        res.append(prefix[n]*postfix[n+1])
+        n += 1
 
     return res
